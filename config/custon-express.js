ba2d35fb4420ae1b7019f1af9d2465306d22ac5b
DouglasPinheiro <douglaspinheiro@id.uff.br>
2017-12-20 20:19:31 -0200

    Configurando os DAO pagamentos

--- config/custon-express.js
+++ config/custon-express.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = () => {
     var app = express();
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
 
+    //validador dos dados recebidos
+    app.use(expressValidator());
+    
+
     consign()
         .include('routes')
+        .then('persistencia')
         .into(app);
 
     return app
--- routes/pagamentos.js
+++ routes/pagamentos.js
@@ -10,11 +10,39 @@ module.exports = (app) => {
         var pagamento = req.body;
         console.log('processando uma requisicao de um novo pagamento');
 
+
+        req.assert('forma_dePagamento','Forma de pagamento é obrigatório').notEmpty();
+        req.assert('valor','Valor é obrigatorio e deve ser um decimal').notEmpty().isFloat();
+
+        var erros = req.validationErros();
+
+        if(erros){
+            console.log('Erros de validação encontrados: '+erros);
+            res.status(400).send(erros);
+            return;
+        }
+
         pagamento.status='criado';
-        pagamento.data = new Date();
-        
-        res.send(pagamento);
-        
+        pagamento.data = new Date;
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.salva(pagamento, (erro, resultado) => {
+
+            if(erro){
+                //caso de erro, altero o status para 400 e envio o erro
+                console.log('erro ao inserir no banco: '+erro);
+                
+                res.status(400).send(erro)
+
+            } else {
+                console.log('Pagamento criado');
+                res.json(pagamento);
+            }
+
+            
+        });
     })
 }
 
