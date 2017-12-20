77b244f3888455488937c5dc52b9bb7eb5b11ba0
DouglasPinheiro <douglaspinheiro@id.uff.br>
2017-12-20 15:14:30 -0200

    Setando o status e a data de pagamento

--- routes/pagamentos.js
+++ routes/pagamentos.js
@@ -8,8 +8,12 @@ module.exports = (app) => {
 
     app.post('/pagamentos/pagamento', (req,res) => {
         var pagamento = req.body;
-        console.log(pagamento);
-        res.send('ok');
+        console.log('processando uma requisicao de um novo pagamento');
+
+        pagamento.status='criado';
+        pagamento.data = new Date();
+        
+        res.send(pagamento);
         
     })
 }
