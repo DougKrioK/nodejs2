9f83647cd2a85a4646d0021b96c018056b4f21a2
DougKrioK <douglaspinheiro@id.uff.br>
2017-12-20 11:30:43 -0200

    .

--- routes/pagamentos.js
+++ routes/pagamentos.js
@@ -1,8 +1,16 @@
 module.exports = (app) => {
+
     app.get('/pagamentos',(req,res) => {
         console.log('recebida requisição de pagamentos');
         res.send('oks')
         
     });
+
+    app.post('/pagamentos/pagamento', (req,res) => {
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('ok');
+        
+    })
 }
 
